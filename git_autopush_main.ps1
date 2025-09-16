1c8894e6ba7e01a7ad9bbdc88e6cb07cae59c73c
ssoj13 <96214515+ssoj13@users.noreply.github.com>
2025-09-15 21:58:14 -0700

    Actions

--- git_autopush_main.ps1
+++
@@ -1,9 +0,0 @@
-$commitMessage = Read-Host "Enter commit message"
-Write-Host "Committing `"$commitMessage`""
-git add -A .
-if ($LASTEXITCODE -eq 0) {
-    git commit -a -m "$commitMessage"
-    if ($LASTEXITCODE -eq 0) {
-        git push --recurse-submodules=on-demand --set-upstream origin main
-    }
-}
